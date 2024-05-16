be34c7db04d9ec8f15b8506f199fc9a931a06817
albus-daniel96 <63167677+albus-daniel96@users.noreply.github.com>
2024-05-16 19:09:46 +0530

    Create storage-container-report.ps1

---
+++ automation/storage-container-report.ps1
@@ -0,0 +1,45 @@
+# Define the storage account details
+$sub = "Sime Darby Plantation (Azure)"
+$containerReport = "Container_Report_"+$date+".csv"
+$storageAccounts = Get-AzStorageAccount
+
+foreach($storageAccount in $storageAccounts){
+
+# Get the storage account context
+$storageAccountContext = $storageAccount.Context
+
+# Get the list of containers
+$containers = Get-AzStorageContainer -Context $storageAccountContext
+
+# Loop through each container
+foreach ($container in $containers) {
+
+    # Get the latest blob uploaded to the container
+    $latestBlob = Get-AzStorageBlob -Container $container.Name -Context $storageAccountContext | Sort-Object -Property LastModified -Descending | Select-Object -First 1
+
+    # Output the details of the latest blob
+    if ($latestBlob) {
+       $BlobName = $($latestBlob.Name)
+        $BlobSize = $($latestBlob.Length)
+        $BolbSize_GB = $BlobSize / 1GB
+        $Last_Modified = $($latestBlob.LastModified)
+    } else {
+        $BlobName = "N/A"
+        $BolbSize_GB = "N/A"
+        $Last_Modified = "N/A"
+    }
+
+$ContainerDetails = "" | Select-Object StorageAccount_Name,Container_Name,Subscription,Location,Blob_Name,Size_Container_GB,Last_Modified_Date
+$ContainerDetails.StorageAccount_Name = $storageAccount.StorageAccountName
+$ContainerDetails.Container_Name = $container.Name
+$ContainerDetails.Subscription = $sub
+$ContainerDetails.Location = $storageAccount.Location
+$ContainerDetails.Blob_Name = $BlobName
+$ContainerDetails.Size_Container_GB = $BolbSize_GB
+$ContainerDetails.Last_Modified_Date=$Last_Modified
+
+#Write-Output "Total data volume in the storage account $storageAccount : $sizeInGB GB"
+
+Export-Csv -InputObject $ContainerDetails -path $containerReport -Append -NoTypeInformation -Force
+}
+}
